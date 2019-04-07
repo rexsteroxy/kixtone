ba27155b75a9f23c48827b7424c851c85023f20d
rexsteroxy <rexsteroxy22@gmail.com>
2019-04-08 00:58:17 +0100

    code push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>FindMyHouse</title>
+    <title>Kiztone</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
